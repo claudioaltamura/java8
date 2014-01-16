363ec7a0fcfd047d9e23ba1218b3f5e07ef78f16
Claudio Altamura <claudio@claudioaltamura.de>
2014-01-16 14:13:15 +0100

    add Comparator Example

---
+++ lambda-partone/src/de/claudioaltamura/java8/lambda/partone/ComparatorExample.java
@@ -0,0 +1,32 @@
+package de.claudioaltamura.java8.lambda.partone;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+
+public class ComparatorExample {
+
+	public static void main(String[] args) {
+		//Setup
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(4);
+		list.add(3);
+		
+		//classic way
+		list.sort(new Comparator<Integer>(){
+
+			@Override
+			public int compare(Integer a, Integer b) {
+				return a - b;
+			}
+		});
+		System.out.println(list);
+		
+		//lambda way
+		list.sort((Integer a, Integer b) -> {
+			return b - a;
+		});
+		System.out.println(list);
+		
+	}
+}
