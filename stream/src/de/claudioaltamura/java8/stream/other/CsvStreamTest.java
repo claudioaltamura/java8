6dde7f363f977431bc439ba17657fdfd9ee70182
Claudio Altamura <claudio@claudioaltamura.de>
2014-04-28 15:53:05 +0200

    stream example

---
+++ stream/src/de/claudioaltamura/java8/stream/other/CsvStreamTest.java
@@ -0,0 +1,26 @@
+package de.claudioaltamura.java8.stream.other;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * From http://www.adam-bien.com/roller/abien/entry/java_8_reducing_a_list
+ */
+public class CsvStreamTest {
+
+    @Test
+    public void listToString() {
+        List<String> mascots = new ArrayList<>();
+        mascots.add("duke");
+        mascots.add("juggy");
+
+        String expected = "duke,juggy";
+        String actual = mascots.stream().
+                reduce((t, u) -> t + "," + u).
+                get();
+        assertThat(actual, is(expected));
+    }
+}
